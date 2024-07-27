5c5fc8dd46b57bcd5bf3acb0b637b73d6dea78c2
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-07-27 21:21:33 +0700

    Create listertracficnorkom.c

---
+++ Test/listertracficnorkom.c
@@ -0,0 +1,8 @@
+void CLASS kheg interpolate (int pass){
+  ushort min, max, upho, cpho;
+  short allhex[3][3][2][8] *hex;
+  float (*drv) [TS][TS], di 6], tr;
+  char (*homo) [TS][TS], *buffen;
+  ushort ("rgb)[TS][TS][3], (*rix)[3], ("pix)[4];
+  cielab (0,0);
+}
