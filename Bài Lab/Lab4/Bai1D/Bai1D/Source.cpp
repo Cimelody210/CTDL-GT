3579c7f6fc1e3b4b8d3b67a8c51b22382cc4f405
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-09-26 15:08:34 +0700

    Add files via upload

---
+++ Bài Lab/Lab4/Bai1D/Bai1D/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include<fstream>
+#include <iomanip>
+
+using namespace std;
+
+#include "menu.h"
+#include "thuvien.h"
+
+int main() {
+	XuLyMenu();
+	return 0;
+}
