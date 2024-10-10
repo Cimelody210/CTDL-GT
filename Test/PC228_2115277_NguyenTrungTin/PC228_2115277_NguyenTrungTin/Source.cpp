25f1397990a8dffe3024707ce809c2d5cfcac7d8
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-10-10 15:07:47 +0700

    Bai kt 10/10

---
+++ Test/PC228_2115277_NguyenTrungTin/PC228_2115277_NguyenTrungTin/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<iomanip>
+#include <string>
+
+using namespace std;
+
+#include "2115277_ThuVien.h"
+#include "2115277_Menu.h"
+
+int main() {
+	int n = 0;
+	SinhVien ds[100];
+	XuLyMenu(ds, n);
+
+	system("pause");
+	return 0;
+}
